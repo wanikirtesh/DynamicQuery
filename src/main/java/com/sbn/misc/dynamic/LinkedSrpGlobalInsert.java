207d89d01f1069642bea5e3259eef69abdfcd39a
Kirtesh Wani <wanikirtesh@gmail.com>
2021-10-18 11:22:38 +0530

    seasons SRP

--- src/main/java/com/sbn/misc/dynamic/LinkedSrpGlobalInsert.java
+++ src/main/java/com/sbn/misc/dynamic/LinkedSrpGlobalInsert.java
@@ -9,29 +9,60 @@ import java.util.List;
 
 public class LinkedSrpGlobalInsert {
     public static void main(String[] args) throws Exception {
+        //globalSRP();
+        activeSRP();
+    }
+
+    private static void globalSRP() throws Exception {
         ConnectionManager cm = new ConnectionManager("mn4sg3xdbsw101.ideasstg.int", "master", "sa", "IDeaS123");
         List<String> lines = Files.readAllLines(Paths.get("C:\\Users\\idnkiw\\OneDrive - SAS\\Desktop\\HCRS_SRP_Derivation_20210427.psv"));
         String strQuery = "Insert InTo [Ratchet].[dbo].Client_Linked_SRP_Mappings (Property_Code,SRP_Code,Linked_Srp_Code,Offset_Type,Yield_As,Created_DTTM,External_System) values (?,?,?,?,?,CURRENT_TIMESTAMP,'HCRS')";
         for(int pid=22001;pid<=22001;pid++) {
             String delQuery = "Delete from [Ratchet].[dbo].Client_Linked_SRP_Mappings where Property_Code = '"+pid+"'";
             cm.Execute(delQuery);
-            PreparedStatement myStements = cm.getPreparedStatementForQuery(strQuery);
+            PreparedStatement myStatements = cm.getPreparedStatementForQuery(strQuery);
             int i=0;
             for (String line : lines) {
                 String[] cols = line.split("\\|");
                 if (i != 0) {
-                    myStements.setString(1, ""+pid);
+                    myStatements.setString(1, ""+pid);
                     for(int j=1;j<=4;j++) {
-                        myStements.setString(j+1, cols[j]);
+                        myStatements.setString(j+1, cols[j]);
                     }
-                    myStements.addBatch();
-
+                    myStatements.addBatch();
                 }
                 i++;
-            }
-            myStements.executeBatch();
-            myStements.close();
-        }
 
+            }
+            myStatements.executeBatch();
+            myStatements.close();
+        }
     }
+
+    private static void activeSRP() throws Exception {
+        ConnectionManager cm = new ConnectionManager("mn4sg3xdbsw101.ideasstg.int", "master", "sa", "IDeaS123");
+        List<String> lines = Files.readAllLines(Paths.get("C:\\Users\\idnkiw\\OneDrive - SAS\\Desktop\\HCRS_SRP_Derivation_20210427.psv"));
+        String strQuery = "Insert InTo [Ratchet].[dbo].Client_Active_Srps (Property_Code,SRP_Code,Linked_Srp_Code,Start_Date,End_Date,Created_DTTM,External_System) values (?,?,?,?,?,CURRENT_TIMESTAMP,'HCRS')";
+        for(int pid=22001;pid<=22001;pid++) {
+            String delQuery = "Delete from [Ratchet].[dbo].Client_Linked_SRP_Mappings where Property_Code = '"+pid+"'";
+            cm.Execute(delQuery);
+            PreparedStatement myStatements = cm.getPreparedStatementForQuery(strQuery);
+            int i=0;
+            for (String line : lines) {
+                String[] cols = line.split("\\|");
+                if (i != 0) {
+                    myStatements.setString(1, ""+pid);
+                    for(int j=1;j<=4;j++) {
+                        myStatements.setString(j+1, cols[j]);
+                    }
+                    myStatements.addBatch();
+                }
+                i++;
+
+            }
+            myStatements.executeBatch();
+            myStatements.close();
+        }
+    }
+
 }
