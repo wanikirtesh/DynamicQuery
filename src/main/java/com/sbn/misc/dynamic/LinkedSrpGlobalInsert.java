d860be9a9e8315e84d1a4e2b85ce3aea370da76f
Kirtesh Wani <wanikirtesh@gmail.com>
2021-10-19 16:22:07 +0530

    adding Parameters

--- src/main/java/com/sbn/misc/dynamic/LinkedSrpGlobalInsert.java
+++ src/main/java/com/sbn/misc/dynamic/LinkedSrpGlobalInsert.java
@@ -9,18 +9,19 @@ import java.util.List;
 
 public class LinkedSrpGlobalInsert {
     public static void main(String[] args) throws Exception {
-        Object[][] data = {{"SEAHM",20221,20240}};
+        Object[][] data = {{"SEAHM",20221,20240,"PCRS"}};
         //globalSRP();
         for (Object[] datum : data) {
-            activeSRP(datum[0].toString(),(int)datum[1],(int)datum[2]);
+            derivationSRP(datum[0].toString(),(int)datum[1],(int)datum[2],datum[3].toString());
+            activeSRP(datum[0].toString(),(int)datum[1],(int)datum[2],datum[3].toString());
         }
 
     }
 
-    private static void globalSRP(String prodPropertyCode, int startProperty, int endProperty) throws Exception {
+    private static void derivationSRP(String prodPropertyCode, int startProperty, int endProperty, String integration) throws Exception {
         ConnectionManager cm = new ConnectionManager("mn4sg3xdbsw101.ideasstg.int", "master", "sa", "IDeaS123");
         List<String> lines = Files.readAllLines(Paths.get("C:\\Users\\idnkiw\\OneDrive - SAS\\Desktop\\HCRS_SRP_Derivation_20210427.psv"));
-        String strQuery = "Insert InTo [Ratchet].[dbo].Client_Linked_SRP_Mappings (Property_Code,SRP_Code,Linked_Srp_Code,Offset_Type,Yield_As,Created_DTTM,External_System) values (?,?,?,?,?,CURRENT_TIMESTAMP,'HCRS')";
+        String strQuery = "Insert InTo [Ratchet].[dbo].Client_Linked_SRP_Mappings (Property_Code,SRP_Code,Linked_Srp_Code,Offset_Type,Yield_As,Created_DTTM,External_System) values (?,?,?,?,?,CURRENT_TIMESTAMP,'"+integration+"')";
         for(int pid=startProperty;pid<=endProperty;pid++) {
             String delQuery = "Delete from [Ratchet].[dbo].Client_Linked_SRP_Mappings where Property_Code = '"+pid+"'";
             cm.Execute(delQuery);
@@ -36,19 +37,18 @@ public class LinkedSrpGlobalInsert {
                     myStatements.addBatch();
                 }
                 i++;
-
             }
             myStatements.executeBatch();
             myStatements.close();
         }
     }
 
-    private static void activeSRP(String prodPropertyCode, int startProperty, int endProperty) throws Exception {
+    private static void activeSRP(String prodPropertyCode, int startProperty, int endProperty, String integration) throws Exception {
         ConnectionManager cm = new ConnectionManager("mn4sg3xdbsw101.ideasstg.int", "master", "sa", "IDeaS123");
         List<String> lines = Files.readAllLines(Paths.get("C:\\Users\\idnkiw\\OneDrive - SAS\\Desktop\\HCRS_SRP_Derivation_20210427.psv"));
-        String strQuery = "Insert InTo [Ratchet].[dbo].Client_Active_Srps (Property_Code,SRP_Code,Linked_Srp_Code,Start_Date,End_Date,Created_DTTM,External_System) values (?,?,?,?,?,CURRENT_TIMESTAMP,'HCRS')";
+        String strQuery = "Insert InTo [Ratchet].[dbo].Client_Active_Srps (Property_Code,SRP_Code,Start_Date,End_Date,Created_DTTM,External_System) values (?,?,?,?,CURRENT_TIMESTAMP,'"+integration+"')";
         for(int pid=startProperty;pid<=endProperty;pid++) {
-            String delQuery = "Delete from [Ratchet].[dbo].Client_Linked_SRP_Mappings where Property_Code = '"+pid+"'";
+            String delQuery = "Delete from [Ratchet].[dbo].Client_Active_Srps where Property_Code = '"+pid+"'";
             cm.Execute(delQuery);
             PreparedStatement myStatements = cm.getPreparedStatementForQuery(strQuery);
             int i=0;
@@ -62,7 +62,6 @@ public class LinkedSrpGlobalInsert {
                     myStatements.addBatch();
                 }
                 i++;
-
             }
             myStatements.executeBatch();
             myStatements.close();
